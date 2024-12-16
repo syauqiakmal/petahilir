e196c5489976202f0554bed7e025395e2052c9dd
edosyhptra <edo23syahputra@gmail.com>
2024-12-16 20:58:25 +0700

    update cors

--- Backend/api.py
+++ Backend/api.py
@@ -52,7 +52,7 @@ app = FastAPI()
 
 origins = [
     "*",
-    "http://petahilirisasi.id",  # Allow only this origin
+    # "http://petahilirisasi.id",  # Allow only this origin
     "https://petahilirisasi.id",
     "http://localhost",  # Allow localhost for development
 ]
